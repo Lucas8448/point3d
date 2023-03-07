dca678c1cf0d2188230d90abee9d3771fbd654c1
Lucas Bateson <lucas.bateson@gmail.com>
2023-03-07 11:23:39 +0100

    Download url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Lucas Bateson',                   # Type in your name
   author_email = 'lucas.bateson@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Lucas8448/point3d',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Lucas8448/point3d/archive/refs/tags/v_0.1.tar.gz',    # I explain this later on
   keywords = ['3d', '2d', 'matrix', 'conversion'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
